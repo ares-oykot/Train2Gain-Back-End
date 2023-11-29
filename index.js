cc690fd287a9a42e760e4651ca299a3c7f9e8f04
ares-oykot <soykotislam131415@gmail.com>
2023-11-29 08:57:23 +0600

    added basic express file

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.202owzh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        
+
+
+        
+
+
+
+
+
+
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', async (req, res) => {
+    res.send("Train2Gain is running");
+});
+app.listen(port, () => {
+    console.log(`Train2Gain is running on port: ${port}`);
+});
