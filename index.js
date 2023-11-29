df2b13d23a047c34e6e90da4839b0f2cf03d988e
ares-oykot <soykotislam131415@gmail.com>
2023-11-29 09:08:50 +0600

    get users from database from database for verify admin

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ async function run() {
             res.send(result);
         }); 
 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            // if (email !== req.decoded.email) {
+            //     return res.status(403).send({ message: 'forbidden access' })
+            // }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            };
+            res.send({ admin });
+        });
+
 
         app.post('/beATrainer', async (req, res) => {
             const trainerInfo = req.body;
